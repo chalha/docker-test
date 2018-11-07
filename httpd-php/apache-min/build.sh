edba4e83b6f684ad54e642e18acc33c6cbdc412f
me <me@rbkhaugdev01.rbk.intra>
2018-11-07 15:11:19 +0100

    .

---
+++ httpd-php/apache-min/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build --no-cache -t apache-min:v1 -t apache-min --rm --force-rm .
--- httpd-php/installscripts/php.sh
+++ httpd-php/installscripts/php.sh
@@ -4,11 +4,11 @@ PHP_FILE="php-$PHP_VERSION.tar.bz2"
 PHP_URL="http://de2.php.net/get/$PHP_FILE/from/this/mirror"
 
 apk add libxml2-dev
-
+cd /tmp
 wget $PHP_URL -O $PHP_FILE || exit 1
 mkdir -p src
-tar xjf $HTTPD_FILE --directory src
-cd "src/php-$VERSION"
+tar xjf $PHP_FILE --directory src
+cd "src/php-$PHP_VERSION"
 ./configure --with-apxs2=/usr/local/apache2/bin/apxs 
 
 make -j "$(nproc)"
